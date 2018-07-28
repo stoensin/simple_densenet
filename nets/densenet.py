699c5971641d3b97b32ce99693c69e8dd5877674
Yang <532383365@qq.com>
2018-07-28 20:57:54 +0800

    Update densenet.py

--- nets/densenet.py
+++ nets/densenet.py
@@ -61,7 +61,7 @@ def densenet(images, num_classes=1001, is_training=False,
       end_points: a dictionary from components of the network to the corresponding
         activation.
     """
-    growth = 40
+    growth = 32
     compression_rate = 0.5
 
     def reduce_dim(input_feature):
